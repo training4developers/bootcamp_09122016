a44e3ca558ae16a2051499fc76d768203614a970
Eric W. Greene <eric@ericwgreene.com>
2016-09-16 16:35:50 -0700

    added day 5 code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,1 +1,50 @@
-//import '../css/styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			newItem: '',
+			items: this.props.items.concat()
+		};
+		this.onChange = this.onChange.bind(this);
+		this.onClick = this.onClick.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	onClick() {
+		this.setState({
+			items: this.state.items.concat(this.state.newItem),
+			newItem: ''
+		});
+	}
+
+	render() {
+		const message = 'Websites We Like To Visit!';
+		return <div className="fun-list">
+			<h1>{message}</h1>
+			<ul>{this.state.items.map(item => <li>{item}</li>)}</ul>
+			<label htmlFor="new-item">New Item</label>
+			<input type="text" id="new-item" name="newItem" value={this.state.newItem} onChange={this.onChange} />
+			<button onClick={this.onClick}>Add Item</button>
+		</div>;
+	}
+
+}
+
+fetch('/api/widgets').then(res => res.json())
+	.then(results =>
+		ReactDOM.render(<HelloWorld items={results.map(widget => widget.name)} />,
+			document.querySelector('main')));
+
+
+
+
+
+
