428266d894f86b8cf960082c321771afb66b7408
Eric W. Greene <eric@ericwgreene.com>
2016-09-21 13:40:38 -0700

    added add/sub code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,237 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class BaseFormComponent extends React.Component {
+import { createStore } from 'redux';
 
-	constructor(props) {
-		super(props);
+function reducer(state = 0, action) {
 
-		this.onChange = this.onChange.bind(this);
-	}
+	console.log('running reducer');
 
-	onChange(e) {
-
-		this.setState({
-			[e.target.name]: e.target.multiple
-				? Array.from(e.target.options)
-					.filter(option => option.selected).map(option => option.value) 
-				: e.target.type === 'checkbox'
-					? e.target.checked
-					: e.target.value
-		});
+	switch(action.type) {
+		case 'add':
+			return state + action.value;
+		case 'subtract':
+			return state - action.value;
+		default:
+			return state;
 	}
 }
 
-class ToDoItemView extends BaseFormComponent {
+const store = createStore(reducer);
 
-	constructor(props) {
-		super(props);
+store.subscribe(() => {
 
-		this.state = {
-			completed: props.todo.completed
-		};
+	console.log('running subscribed function');
+	console.log(store.getState());
 
-		this.editToDo = this.editToDo.bind(this);
-	}
+});
 
-	editToDo() {
-		this.props.editToDo(this.props.todo.id);
-	}
-
-	render() {
-		return <li>
-			<input type="checkbox" name="completed"
-				id="completed" value={this.state.completed}
-				onChange={this.onChange} />
-			<span onClick={this.editToDo}>{this.props.todo.task}</span>
-		</li>;
-	}
-
-}
-
-ToDoItemView.propTypes = {
-	editToDo: React.PropTypes.func,
-	todo: React.PropTypes.shape({
-		id: React.PropTypes.number,
-		task: React.PropTypes.string,
-		due: React.PropTypes.instanceOf(Date)
-	}).isRequired
-};
-
-class ToDoItemEdit extends BaseFormComponent {
-
-	constructor(props) {
-
-		super(props);
-
-		this.state = {
-			completed: props.todo.completed,
-			task: props.todo.task
-		};
-
-		this.doneToDo = this.doneToDo.bind(this);
-		this.cancelEdit = this.cancelEdit.bind(this);
-	}
-
-	doneToDo(e) {
-
-		switch(e.keyCode) {
-			case 13:
-				this.props.saveToDo({
-					id: this.props.todo.id,
-					task: this.state.task,
-					completed: this.state.completed
-				});
-				break;
-			case 27:
-				this.props.cancelEditToDo();
-				break;
-		}
-
-	}
-
-	cancelEdit() {
-		this.props.cancelEditToDo();
-	}
-
-	componentDidMount() {
-		this._taskInput.focus();
-	}
-
-	render() {
-		return <li>
-			<input type="checkbox" name="completed"
-				id="completed" value={this.state.completed}
-				onChange={this.onChange} />
-			<input type="text" name="task" id="task"
-				value={this.state.task} ref={c => this._taskInput = c}
-				onChange={this.onChange} onKeyUp={this.doneToDo} onBlur={this.cancelEdit} />
-		</li>;
-	}
-
-}
-
-class ToDoList extends React.Component {
-
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			editToDoId: 0
-		};
-
-		this.editToDo = this.editToDo.bind(this);
-		this.saveToDo = this.saveToDo.bind(this);
-		this.cancelEditToDo = this.cancelEditToDo.bind(this);
-	}
-
-	editToDo(toDoId) {
-
-		this.setState({
-			editToDoId: toDoId
-		});
-
-	}
-
-	saveToDo(todo) {
-
-		this.props.saveToDo(todo);
-
-		this.setState({
-			editToDoId: 0
-		});
-	}
-
-	cancelEditToDo() {
-		this.setState({
-			editToDoId: 0
-		});
-	}
-
-	render() {
-
-		return <ul>
-			{this.props.todos.map(todo => this.state.editToDoId === todo.id
-				? <ToDoItemEdit key={todo.id} todo={todo} saveToDo={this.saveToDo} cancelEditToDo={this.cancelEditToDo} />
-				: <ToDoItemView key={todo.id} todo={todo} editToDo={this.editToDo} />)}
-		</ul>;
-
-	}
-
-}
-
-ToDoList.propTypes = {
-	todos: React.PropTypes.array,
-	saveToDo: React.PropTypes.func
-};
-
-class ToDos extends React.Component {
-
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			todos: [
-				{id: 1, task: 'Task 1', completed: true },
-				{id: 2, task: 'Task 2', completed: false },
-				{id: 3, task: 'Task 3', completed: false }
-			]
-		};
-
-		this.saveToDo = this.saveToDo.bind(this);
-	}
-
-	saveToDo(todo) {
-
-		const todoIndex = this.state
-			.todos.indexOf(this.state.todos.find(t => t.id === todo.id));
+console.log('dispatching...');
+store.dispatch({
+	type: 'add',
+	value: 2
+});
 
 
-		this.setState({
-			todos: this.state.todos
-				.slice(0, todoIndex).concat(todo)
-				.concat(this.state.todos.slice(todoIndex + 1))
-		});
-	}
 
-	render() {
-		return <ToDoList todos={this.state.todos} saveToDo={this.saveToDo} />;
-	}
 
-}
 
-ReactDOM.render(<ToDos />, document.querySelector('main'));
 
-// class ViewRow extends React.Component {
 
-// 	render() {
 
-// 		return <tr>
-// 			<td>{this.props.widget.name}</td>
-// 			<td>{this.props.widget.color}</td>
-// 			<td>{this.props.widget.size}</td>
-// 			<td>{this.props.widget.quantity}</td>
-// 		</tr>;
 
+
+
+
+
+
+
+
+// console.log([1,2,3,4,5].reduce(function(previousValue, nextValue) {
+// 	console.log(previousValue, nextValue);
+// 	return previousValue + nextValue;
+// }));
+
+// const actions = [1,2,3,4,5];
+
+// const initialState = 0;
+
+// console.log(initialState);
+
+// // const finalState = actions.reduce(function(state, action) {
+// // 	console.log(state, action);
+// // 	return state + action;
+// // }, initialState);
+
+// function reducer(state, action) {
+
+// 	switch(action.type) {
+// 		case 'add':
+// 			return state + action.value;
+// 		case 'subtract':
+// 			return state - action.value;
+// 		default:
+// 			return state;
 // 	}
-
-
 // }
 
-// class EditRow extends React.Component {
-
-// 	render() {
-
-// 		return <tr>
-// 			<td><input type="text" value={this.state.widgetName} onChange={this.onChange} /></td>
-// 			<td><input type="text" value={this.state.widgetColor} onChange={this.onChange} /></td>
-// 			<td><input type="text" value={this.state.widgetSize} onChange={this.onChange} /></td>
-// 			<td><input type="text" value={this.state.widgetQuantity} onChange={this.onChange} /></td>
-// 		</tr>;
-
-// 	}
-
-
-// }
-
-
+// let state = reducer(initialState, { type: 'add', value: 1 }); // represents a user action
+// console.log(state);
+// state = reducer(state, { type: 'subtract', value: 2 }); // represents a user action
+// console.log(state);
+// state = reducer(state, { type: 'add', value: 3 }); // represents a user action
+// console.log(state);
+// state = reducer(state, { type: 'add', value: 4 }); // represents a user action
+// console.log(state);
+// state = reducer(state, { type: 'subtract', value: 5 }); // represents a user action
+// console.log(state);
